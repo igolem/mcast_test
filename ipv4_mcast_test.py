98ddff4607b0045e16ae20967871a95dd31ddc73
Jason Mueller <15204665+igolem@users.noreply.github.com>
2019-04-04 23:31:55 -0500

    added logging to file and ability to remove interval

--- ipv4_mcast_test.py
+++ ipv4_mcast_test.py
@@ -3,7 +3,7 @@
 # script: ipv4_mcast_test.py
 # author: jason mueller
 # created: 2019-01-28
-# last modified: 2019-03-02
+# last modified: 2019-04-04
 #
 # purpose:
 # Script to test basic multicast functionality.
@@ -52,15 +52,21 @@ def get_cli_switches():
                             help = 'set UDP unprivileged port for source or listener.')
     cli_parser.add_argument('-i',
                             dest = 'interval',
-                            default = 2,
+                            default = 1,
                             type = int,
                             help = 'set delay in seconds between sending messages ' +
-                                   'when script is a multicast source (range: 1-60).')
+                                   'when script is a multicast source (range: 0-60).')
     cli_parser.add_argument('-t',
                             dest = 'ttl',
                             default = 10,
                             type = int,
                             help = 'set TTL in IP header (range: 1-31).')
+
+    cli_parser.add_argument('-l',
+                            dest = 'log',
+                            action='store_true',
+                            help = 'log sent/received messages.')
+
 
     cli_args = vars(cli_parser.parse_args())
     return(cli_args)
@@ -150,9 +156,15 @@ def santize_args(cli_args):
     else:
         args['port'] = cli_args['port']
 
+    # check if logging sent/received messages is set
+    if cli_args['log']:
+        args['log'] = True
+    else:
+        args['log'] = False
+
     if cli_args['source']:
         # verify interval is between 1-60
-        if (cli_args['interval'] < 1 or cli_args['interval'] > 60):
+        if (cli_args['interval'] < 0 or cli_args['interval'] > 60):
             args['interval'] = 2
             print('Specified interval was not in allowed range of 1-60, using 2.')
         else:
@@ -164,7 +176,7 @@ def santize_args(cli_args):
             print('Specified TTL was not in allowed range of 1-31, using 10.')
         else:
             args['ttl'] = cli_args['ttl']
-
+    
     return args
 
 
@@ -182,6 +194,12 @@ def mcast_listener(args):
                         socket.IP_ADD_MEMBERSHIP,
                         listener)
 
+        if args['log']:
+            recv_msgs_file = 'mcast_recv_message.txt'
+            recv_fh = open(recv_msgs_file, 'a')
+            print('\nMulticast messages written to file: {file}.'
+                  .format(file=recv_msgs_file))
+    
         print('\nMulticast listener invoked at {time}.'
               .format(time=datetime.datetime.now()))
         print('Listening to multicast group {group_ip}:{port}.\n'
@@ -190,12 +208,18 @@ def mcast_listener(args):
         # print data received on socket
         while True:
             recv_msg = sock.recv(1024)
-            print('{time}, received message to mcast group {group}:{port}: "{message}"'
-                  .format(time=datetime.datetime.now(), group=args['group'],
-                          port=args['port'], message=recv_msg))
+            recv_time = datetime.datetime.now()
+            print('{timestamp}, received message to mcast group {group}:{port}: "{message}"'
+                  .format(timestamp=recv_time, group=args['group'],
+                          port=args['port'], message=str(recv_msg)[2:-1]))
+        
+            if args['log']:
+                recv_fh.write('{timestamp},{message}\n'
+                    .format(timestamp = recv_time, message = str(recv_msg)[2:-1]))
+
     except:
         if not KeyboardInterrupt:
-            print('\nScript exited due unexpected error occurred.\n')
+            print('\nScript exited due to an unexpected error.\n')
 
 
 # multicast source
@@ -205,6 +229,11 @@ def mcast_source(args):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, args['ttl'])
 
+        if args['log']:
+            sent_msgs_file = 'mcast_sent_message.txt'
+            sent_fh = open(sent_msgs_file, 'a')
+            print('\nMulticast messages written to file: {file}.'
+                  .format(file=sent_msgs_file))
         print('\nMulticast source invoked at {time}.'
               .format(time=datetime.datetime.now()))
         print('Sourcing timestamp message to {group_ip}:{port} every {intrvl} seconds.\n'
@@ -222,7 +251,11 @@ def mcast_source(args):
             sock.sendto(send_msg.encode('utf-8'), (args['group'], args['port']))
             print('Sent message to {group_ip}:{port}: "{message}."'
                   .format(group_ip=args['group'], port=args['port'], message=send_msg))
-            time.sleep(args['interval'])
+            if args['log']:
+                sent_fh.write('{message}\n'
+                    .format(message = send_msg ))
+            if args['interval'] > 0:
+                time.sleep(args['interval'])
 
     except:
         if not KeyboardInterrupt:
